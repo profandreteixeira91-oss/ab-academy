1a1d52dc2510d04de4b9cff6c191288a911e85a4
profandreteixeira91-oss <profandreteixeira91@gmail.com>
2026-09-22 19:04:32 -0300

    Cria footer institucional reutilizável

---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,36 @@
+import '../styles/site-footer.css'
+import logo from '../assets/logo_abacademy.png'
+
+export default function SiteFooter() {
+  return (
+    <footer className="site-footer">
+      <div className="site-footer-container">
+        <div className="site-footer-brand">
+          <a href="/" className="site-footer-logo">
+            <img src={logo} alt="AB Academy" />
+          </a>
+          <p>Inglês e alemão para transformar conhecimento em novas oportunidades.</p>
+        </div>
+
+        <div className="site-footer-links">
+          <a href="/">Início</a>
+          <a href="/matricula">Matrícula</a>
+          <a href="/aluno">Área do aluno</a>
+          <a href="/professor">Portal do professor</a>
+        </div>
+      </div>
+
+      <div className="site-footer-bottom">
+        <span>AB Academy®</span>
+        <span>
+          Desenvolvido por{' '}
+          <a href="https://www.amtsistemas.com.br">AMT Sistemas &amp; Soluções</a>
+        </span>
+        <div className="site-footer-legal">
+          <a href="/termos-de-servico">Termos de Serviço</a>
+          <a href="/politica-privacidade">Política de Privacidade</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
