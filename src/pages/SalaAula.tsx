3cd20980fd0d69b089697a7383cf67dd362cc044
profandreteixeira91-oss <profandreteixeira91@gmail.com>
2026-09-23 20:16:14 -0300

    Padroniza logos dos cabeçalhos

--- src/pages/SalaAula.tsx
+++ src/pages/SalaAula.tsx
@@ -1262,6 +1262,7 @@ export default function SalaAula() {
           <img
             src={logo}
             alt="AB Academy"
+            className="academy-header-logo"
           />
         </div>
 
