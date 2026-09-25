dd9a53bcb19081c18af8e09a9d097f536f131b49
profandreteixeira91-oss <profandreteixeira91@gmail.com>
2026-09-24 22:51:57 -0300

    Atualiza textos e link do rodapé

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -4,8 +4,13 @@ export default function SiteFooter() {
   return (
     <footer className="site-footer">
       <div className="site-footer-bottom">
-        <span>AB Academy todos os direitos reservados</span>
-        <span>Desenvolvido por AMT Sistemas</span>
+        <span><strong>AB Academy Idiomas</strong>® 2026 - <strong>todos os direitos reservados</strong></span>
+        <span>
+          Desenvolvido por AMT Sistemas &amp; Soluções -{' '}
+          <a href="https://www.amtsistemas.com.br" target="_blank" rel="noopener noreferrer">
+            www.amtsistemas.com.br
+          </a>
+        </span>
       </div>
     </footer>
   )
